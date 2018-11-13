a383e63f17a0f9a65f9438eae4b740e36c2dc72b
zengrong <1780297674@qq.com>
2018-11-13 11:45:53 +0800

    细节调整

--- pages/carCustomizationOrderDetail/carCustomizationOrderDetail.js
+++ pages/carCustomizationOrderDetail/carCustomizationOrderDetail.js
@@ -107,9 +107,6 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    this.setData({
-      pageData: {}
-    })
     this.showLoading('页面刷新中')
     this.getDetail(this.data.id)
   },
